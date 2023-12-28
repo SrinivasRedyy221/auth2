061149e08251880bb55d0f1b402b319a3d6fd80c
Srinivas <piserla.srinivas@gmail.com>
2023-12-29 00:58:19 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -11,11 +11,24 @@ connection();
 
 // middlewares
 app.use(express.json());
+
+// Enable CORS for all routes
 app.use(cors());
+
+// Enable CORS for a specific route
+// app.use("/api/auth", cors());
 
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// Add a middleware to handle CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
